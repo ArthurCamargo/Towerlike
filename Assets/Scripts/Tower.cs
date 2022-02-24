6a49ff9a9a7560b10256bf6a8d07af218011fef0
Arthur Vinícius Cunha Camargo <avccamargo@inf.ufrgs.br>
2022-02-24 17:27:28 +0000

    make towers SHOOT pew pew

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,22 +7,69 @@ public class Tower : MonoBehaviour
 {
     public Transform towerPrefab;
     public Transform placeToShoot;
-    public float range;
-    Transform Target;
+    public float range = 5;
+    public Transform target;
+    string enemyTag = "Enemy";
     public Projectile projectile;
     public float msBetweenShoots = 100;
     public float shootSpeed = 5;
     public float damage = 1.0f;
     
-    //See if there is an enemy at sight
-    public void Aim()
-    {
+    float nextShotTime;
+    
+    void Start() {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
-    public void Shoot(Transform Target) {
+    
+    void Update() {
+       if(target == null)
+           return;
+       
+
+        if(Time.time > nextShotTime)
+        {
+            nextShotTime = Time.time + msBetweenShoots/ 1000;
+            Shoot(target);
+        }
+    }
+    //See if there is an enemy at sight
+    public void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        
+        foreach (GameObject enemy in enemies)
+        {
+            float distaceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distaceToEnemy < shortestDistance)
+            {
+                shortestDistance = distaceToEnemy;
+                nearestEnemy = enemy;
+            }
+
+        }
+        
+        if (nearestEnemy != null && shortestDistance < range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    public void Shoot(Transform target) {
         
         Projectile newProjectile = Instantiate(projectile, placeToShoot.position, placeToShoot.rotation) as Projectile;
         newProjectile.SetSpeed(shootSpeed);
-        newProjectile.SetTarget(Target);
+        newProjectile.SetTarget(target);
     }
     public void SetDamage(float newDamage) {
         damage = newDamage;
