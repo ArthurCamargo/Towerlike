97f2f9dbfbc69cdaee95de6bfdeef6bcd7dfdee8
Roger Albuquerque <roger.odahar@gmail.com>
2022-03-10 23:22:31 +0000

    No Comments

--- Assets/Scripts/Itens/CombatItem.cs
+++ Assets/Scripts/Itens/CombatItem.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Combat Item", menuName = "Inventory/Combat Item")]
 public class CombatItem : Item
 {
+
 }
--- Assets/Scripts/Itens/Item.cs
+++ Assets/Scripts/Itens/Item.cs
@@ -2,7 +2,24 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class Item: MonoBehaviour
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
 {
-    public ScriptableItem itemScriptableObject;
+    new public string name = "New Item";
+    /*
+    public enum itemType{
+        Combat,
+        Buff,
+        Tower,
+    };
+    
+    public itemType type = itemType.Combat;
+    */
+    
+    public Sprite icon = null;
+
+    public virtual void Use() {
+        // Use the item
+        Debug.Log("Using " + name);
+    }
 }
