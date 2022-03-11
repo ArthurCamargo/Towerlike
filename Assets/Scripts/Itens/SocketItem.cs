4c5609d42bcd7dea6c3b69472af1c7c8a5210c19
Roger Albuquerque <roger.odahar@gmail.com>
2022-03-11 05:13:34 +0000

    No Comments

---
+++ Assets/Scripts/Itens/SocketItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Socket Item", menuName = "Inventory/Socket Item")]
+public class SocketItem : Item
+{
+
+}
